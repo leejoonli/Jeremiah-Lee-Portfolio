85d23f87fa48193b527396bba52bba2c5135088d
jeremiahlee94 <jhwlee94@gmail.com>
2022-02-04 15:23:17 -0800

    add details to projects component

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -5,10 +5,24 @@ function Projects() {
         <div>
             <div>
                 <div>placeholder project 1</div>
-                <p>Tower of Hanoi is an application based off the mathematical puzzle game "Tower of Hanoi".  Can you complete this game?</p>
+                <div>
+                    <h2>Tower of Hanoi</h2>
+                    <p>Tower of Hanoi is an application based off the real-life mathematical puzzle game "Tower of Hanoi".  Can you complete this challenging game?  Adjust the difficulty and find out how far you can go.  Created in vanilla JavaScript, HTML, and CSS.</p>
+                    <div>
+                        <a>Application</a>
+                        <a>Repository</a>
+                    </div>
+                </div>
             </div>
             <div>
-                <p>description project 2</p>
+                <div>
+                    <h2>Dungeons and Dragons 5e Guide</h2>
+                    <p>This application is a straightforward access point for one of greatest table top role playing games currently in existence, if not THE greatest.  Browse through the application and see what catches your eye.  Built using React, React Bootstrap, React Router, JavaScript, CSS, and deployed on Netlify.</p>
+                    <div>
+                        <a>Application</a>
+                        <a>Repository</a>
+                    </div>
+                </div>
                 <div>placeholder project 2</div>
             </div>
             <div>
