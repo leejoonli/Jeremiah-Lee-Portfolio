fa721e8281272db5bbe049ccf21b27c93d1814b1
jeremiahlee94 <jhwlee94@gmail.com>
2022-02-04 15:40:34 -0800

    add details to navigation component

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 
 function Navigation() {
     return (
-        <div>
-            hello from navigation
-        </div>
+        <>
+            <div>
+                <div>placeholder img</div>
+                <div>
+                    <h2>Skills</h2>
+                    <h2>Projects</h2>
+                    <h2>About</h2>
+                    <h2>CV</h2>
+                    <h2>GitHub</h2>
+                    <h2>LinkedIn</h2>
+                    <h2>Contact</h2>
+                </div>
+            </div>
+        </>
     );
 }
 
