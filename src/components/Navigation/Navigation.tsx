69d34c4733b1ddd689c386a374e892ca05e12d78
jeremiahlee94 <jhwlee94@gmail.com>
2022-02-05 09:07:45 -0800

    add state variable to navigation component

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Navigation() {
+    const [test, setTest] = useState(false);
+
+    const handleClick = (event: any) => {
+        setTest(prevCheck => !prevCheck);
+    }
+
     return (
         <>
             <div>
-                <div>placeholder img</div>
-                <div>
-                    <h2>Skills</h2>
-                    <h2>Projects</h2>
-                    <h2>About</h2>
-                    <h2>CV</h2>
-                    <h2>GitHub</h2>
-                    <h2>LinkedIn</h2>
-                    <h2>Contact</h2>
-                </div>
+                <div onClick={handleClick}>placeholder img</div>
+                {test && (
+                    <>
+                        <div>
+                            <h2>Skills</h2>
+                            <h2>Projects</h2>
+                            <h2>About</h2>
+                            <h2>CV</h2>
+                            <h2>GitHub</h2>
+                            <h2>LinkedIn</h2>
+                            <h2>Contact</h2>
+                        </div>
+                    </>
+                )}
             </div>
         </>
     );
