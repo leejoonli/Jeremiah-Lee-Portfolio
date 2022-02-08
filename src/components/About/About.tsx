51263d809adf086eb13193c0e92d0b86a0e95640
jeremiahlee94 <jhwlee94@gmail.com>
2022-02-08 14:02:55 -0800

    update projects and about component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,11 +5,11 @@ function About() {
     return (
         <div className={styles.aboutContainer}>
             <h1 className={styles.aboutHeader}>About Me</h1>
-            <p>Hello! I'm Jeremiah, a software engineer and doting cat uncle.  When I'm not giving pets to my favorite cat, I'm complimenting other peoples' favorite cat/s.</p>
+            <p className={styles.aboutDesc}>Hello! I'm Jeremiah, a software engineer and doting cat uncle.  When I'm not giving pets to my favorite cat, I'm complimenting other peoples' favorite cat/s.</p>
             <br />
-            <p>After working as a camera assistant, I decided to leave the film industry and pursue a better future self. I always had an interest in the sciences even in my earlier years. But now, that interest has transformed into a fascination.</p>
+            <p className={styles.aboutDesc}>After working as a camera assistant, I decided to leave the film industry and pursue a better future self. I always had an interest in the sciences even in my earlier years. But now, that interest has transformed into a fascination.</p>
             <br />
-            <p>On the never-ending journey of discovery, learning, and growth.  This is me.</p>
+            <p className={styles.aboutDesc}>On the never-ending journey of discovery and growth,  This is me.</p>
         </div>
     );
 }
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Projects.module.css';
-import tower_of_hanoi from './img/tower_of_hanoi.png';
+import towerOfHanoi from './img/tower_of_hanoi.png';
 import dnd from './img/dnd.png';
 import felp from './img/felp.png';
 
@@ -9,11 +9,11 @@ function Projects() {
         <div>
             <div className={styles.projectContainer}>
                 <div className={styles.projectImgContainer}>
-                    <img src={tower_of_hanoi} alt='Application State' className={styles.projectImg} />
+                    <img src={towerOfHanoi} alt='Application State' className={styles.projectImg} />
                 </div>
                 <div className={styles.project}>
                     <h2>Tower of Hanoi</h2>
-                    <p>Tower of Hanoi is an application based off the real-life mathematical puzzle game "Tower of Hanoi".  Can you complete this challenging game?  Adjust the difficulty and find out how far you can go.  Created in vanilla JavaScript, HTML, and CSS.</p>
+                    <p className={styles.projectDesc}>Tower of Hanoi is an application based off the real-life mathematical puzzle game "Tower of Hanoi".  Can you complete this challenging game?  Adjust the difficulty and find out how far you can go.  Created in vanilla JavaScript, HTML, and CSS.</p>
                     <div>
                         <a href='https://leejoonli.github.io/tower-of-hanoi/' target='_blank' rel='noreferrer'>Application</a>
                         <a href='https://github.com/leejoonli/tower-of-hanoi' target='_blank' rel='noreferrer'>Repository</a>
@@ -26,7 +26,7 @@ function Projects() {
                 </div>
                 <div className={styles.project}>
                     <h2>Dungeons and Dragons 5e Guide</h2>
-                    <p>This application is a straightforward access point for one of greatest table top role playing games currently in existence, if not THE greatest.  Browse through the application and see what catches your eye.  Built using React, React Bootstrap, React Router, JavaScript, CSS, and deployed on Netlify.</p>
+                    <p className={styles.projectDesc}>This application is a straightforward access point for one of greatest table top role playing games currently in existence, if not THE greatest.  Browse through the application and see what catches your eye.  Built using React, React Bootstrap, React Router, JavaScript, CSS, and deployed on Netlify.</p>
                     <div>
                         <a href='https://dungeons-and-dragons-5e-guide-6b812b.netlify.app/' target='_blank' rel='noreferrer'>Application</a>
                         <a href='https://github.com/leejoonli/dungeons-and-dragons-5e-guide' target='_blank' rel='noreferrer'>Repository</a>
@@ -39,7 +39,7 @@ function Projects() {
                 </div>
                 <div className={styles.project}>
                     <h2>FELP</h2>
-                    <p>FELP is a blog style food review application to document the hottest spots to go to eat.  The idea is verified locals who have lived in a certain area for an extended period of time would suggest the bets local restaraunts for tourists to go.  The application has full CRUD functionaliy for registered users.  Frontend created using React, Javascript, CSS, with requests handled using Axios.  Backend created with MongoDB, Mongoose, Express, with user authentication being handled by Passport.  Feel like a local even when you're out of town.</p>
+                    <p className={styles.projectDesc}>FELP is a blog style food review application to document the hottest spots to go to eat.  The idea is verified locals who have lived in a certain area for an extended period of time would suggest the bets local restaraunts for tourists to go.  The application has full CRUD functionaliy for registered users.  Frontend created using React, Javascript, CSS, with requests handled using Axios.  Backend created with MongoDB, Mongoose, Express, with user authentication being handled by Passport.  Feel like a local even when you're out of town.</p>
                     <div>
                         <a href='https://felp-official.netlify.app/' target='_blank' rel='noreferrer'>Application</a>
                         <a href='https://github.com/leejoonli/felp-frontend' target='_blank' rel='noreferrer'>Frontend</a>
@@ -51,7 +51,7 @@ function Projects() {
                 <div className={styles.projectImgContainer}>placeholder project 4</div>
                 <div className={styles.project}>
                     <h2>project 4 name</h2>
-                    <p>description project 4</p>
+                    <p className={styles.projectDesc}>description project 4</p>
                 </div>
             </div>
         </div>
