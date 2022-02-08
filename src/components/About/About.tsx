998f6dcff45dcfbad273affe8a3723549c299ab2
jeremiahlee94 <jhwlee94@gmail.com>
2022-02-08 13:33:20 -0800

    update about component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,8 +4,12 @@ import styles from './About.module.css';
 function About() {
     return (
         <div className={styles.aboutContainer}>
-            <h1>About Me</h1>
-            <p>Hello! I'm a software engineer and doting cat uncle with a background in film.  Engrossing myself in such a lively, intense industry instilled the importance of having a mindset to consistently learn new skills, having effective communication, and the ability to exceed presented expectations. My ability to persist through and past obstacles stems from a child-like curiosity and love of learning about computer science and from having the mindset of software engineering being innately exhilarating.</p>
+            <h1 className={styles.aboutHeader}>About Me</h1>
+            <p>Hello! I'm Jeremiah, a software engineer and doting cat uncle.  When I'm not giving pets to my favorite cat, I'm complimenting other peoples' favorite cat/s.</p>
+            <br />
+            <p>After working as a camera assistant, I decided to leave the film industry and pursue a better future self. I always had an interest in the sciences even in my earlier years. But now, that interest has transformed into a fascination.</p>
+            <br />
+            <p>On the never-ending journey of discovery, learning, and growth.  This is me.</p>
         </div>
     );
 }
