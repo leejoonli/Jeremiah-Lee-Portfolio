90ec15265d9fab181bb4cd5c16341e445bc67167
jeremiahlee94 <jhwlee94@gmail.com>
2022-02-07 18:41:12 -0800

    add css to technologies component

--- src/components/Technologies/Technologies.tsx
+++ src/components/Technologies/Technologies.tsx
@@ -15,48 +15,48 @@ function Technologies() {
             <h1>Languages</h1>
             <div className={styles.languages}>
                 <div>
-                    <img src={js} alt="JavaScript Icon" />
-                    <h2>JavaScript</h2>
+                    <img src={js} alt="JavaScript Icon" className={styles.techIcons} />
+                    <h2 className={styles.techHeader}>JavaScript</h2>
                 </div>
                 <div>
                     {/* GET TYPESCRIPT IMG */}
                     {/* <img src={js} alt="Typescript Icon" /> */}
-                    <h2>TypeScript</h2>
+                    <h2 className={styles.techHeader}>TypeScript</h2>
                 </div>
                 <div>
-                    <img src={python} alt="Python Icon" />
-                    <h2>Python</h2>
+                    <img src={python} alt="Python Icon" className={styles.techIcons} />
+                    <h2 className={styles.techHeader}>Python</h2>
                 </div>
                 <div>
-                    <img src={html} alt="HTML Icon" />
-                    <h2>HTML</h2>
+                    <img src={html} alt="HTML Icon" className={styles.techIcons} />
+                    <h2 className={styles.techHeader}>HTML</h2>
                 </div>
                 <div>
-                    <img src={css} alt="CSS Icon" />
-                    <h2>CSS</h2>
+                    <img src={css} alt="CSS Icon" className={styles.techIcons} />
+                    <h2 className={styles.techHeader}>CSS</h2>
                 </div >
             </div>
             <h1>Frameworks</h1>
             <div className={styles.frameworks}>
                 <div>
-                    <img src={react} alt="React Icon" />
-                    <h2>React</h2>
+                    <img src={react} alt="React Icon" className={styles.techIcons} />
+                    <h2 className={styles.techHeader}>React</h2>
                 </div>
                 {/* <div>
                     <img src={css} alt="CSS Icon" />
                     <h2>React Router</h2>
                 </div> */}
                 <div>
-                    <img src={mongodb} alt="MongoDB Icon" />
-                    <h2>MongoDB</h2>
+                    <img src={mongodb} alt="MongoDB Icon" className={styles.techIcons} />
+                    <h2 className={styles.techHeader}>MongoDB</h2>
                 </div>
                 <div>
-                    <img src={express} alt="Express Icon" />
-                    <h2>Express</h2>
+                    <img src={express} alt="Express Icon" className={styles.techIcons} />
+                    <h2 className={styles.techHeader}>Express</h2>
                 </div>
                 <div>
-                    <img src={sql} alt="SQL Icon" />
-                    <h2>SQL</h2>
+                    <img src={sql} alt="SQL Icon" className={styles.techIcons} />
+                    <h2 className={styles.techHeader}>SQL</h2>
                 </div>
             </div>
         </div>
