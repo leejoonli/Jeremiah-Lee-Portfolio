e627cbc550b9f55c357c5a94dd935f7a5f076c97
jeremiahlee94 <jhwlee94@gmail.com>
2022-02-08 18:59:27 -0800

    add padding to about component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,7 +4,7 @@ import styles from './About.module.css';
 function About() {
     return (
         <div className={styles.aboutContainer}>
-            <h1 className={styles.aboutHeader}>About Me</h1>
+            <h1 className={styles.aboutHeader}>ABOUT ME</h1>
             <p className={styles.aboutDesc}>Hello! I'm Jeremiah, a software engineer and doting cat uncle.  When I'm not giving pets to my favorite cat, I'm complimenting other peoples' favorite cat/s.</p>
             <br />
             <p className={styles.aboutDesc}>After working as a camera assistant, I decided to leave the film industry and pursue an opportunity to change my future. I always had an interest in the sciences even in my earlier years, but now that interest has transformed into a fascination.  Coding has taken over the driver's seat and it's full steam ahead.</p>
