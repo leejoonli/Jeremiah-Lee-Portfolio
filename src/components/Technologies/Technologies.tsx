758d865cf1d826862787dbe9fde0e1e5757e4de2
jeremiahlee94 <jhwlee94@gmail.com>
2022-02-08 18:12:44 -0800

    add css to technologies component to space out icons

--- src/components/Technologies/Technologies.tsx
+++ src/components/Technologies/Technologies.tsx
@@ -8,37 +8,37 @@ import react from './img/react.png';
 import mongodb from './img/mongodb.png';
 import express from './img/express.png';
 import sql from './img/sql.png';
+import ts from './img/ts.png';
 
 function Technologies() {
     return (
         <div className={styles.techContainer}>
-            <h1>Languages</h1>
+            <h1 className={styles.techHeading}>LANGUAGES</h1>
             <div className={styles.languages}>
-                <div>
+                <div className={styles.iconContainer}>
                     <img src={js} alt="JavaScript Icon" className={styles.techIcons} />
                     <h2 className={styles.techHeader}>JavaScript</h2>
                 </div>
-                <div>
-                    {/* GET TYPESCRIPT IMG */}
-                    {/* <img src={js} alt="Typescript Icon" /> */}
+                <div className={styles.iconContainer}>
+                    <img src={ts} alt="Typescript Icon" className={styles.techIcons} />
                     <h2 className={styles.techHeader}>TypeScript</h2>
                 </div>
-                <div>
+                <div className={styles.iconContainer}>
                     <img src={python} alt="Python Icon" className={styles.techIcons} />
                     <h2 className={styles.techHeader}>Python</h2>
                 </div>
-                <div>
+                <div className={styles.iconContainer}>
                     <img src={html} alt="HTML Icon" className={styles.techIcons} />
                     <h2 className={styles.techHeader}>HTML</h2>
                 </div>
-                <div>
+                <div className={styles.iconContainer}>
                     <img src={css} alt="CSS Icon" className={styles.techIcons} />
                     <h2 className={styles.techHeader}>CSS</h2>
                 </div >
             </div>
-            <h1>Frameworks</h1>
+            <h1 className={styles.techHeading2}>FRAMEWORKS</h1>
             <div className={styles.frameworks}>
-                <div>
+                <div className={styles.iconContainer}>
                     <img src={react} alt="React Icon" className={styles.techIcons} />
                     <h2 className={styles.techHeader}>React</h2>
                 </div>
@@ -46,15 +46,15 @@ function Technologies() {
                     <img src={css} alt="CSS Icon" />
                     <h2>React Router</h2>
                 </div> */}
-                <div>
+                <div className={styles.iconContainer}>
                     <img src={mongodb} alt="MongoDB Icon" className={styles.techIcons} />
                     <h2 className={styles.techHeader}>MongoDB</h2>
                 </div>
-                <div>
+                <div className={styles.iconContainer}>
                     <img src={express} alt="Express Icon" className={styles.techIcons} />
                     <h2 className={styles.techHeader}>Express</h2>
                 </div>
-                <div>
+                <div className={styles.iconContainer}>
                     <img src={sql} alt="SQL Icon" className={styles.techIcons} />
                     <h2 className={styles.techHeader}>SQL</h2>
                 </div>
