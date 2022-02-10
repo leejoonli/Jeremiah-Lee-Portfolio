525d4de6a08a1919d26655057f5826e4749d9630
jeremiahlee94 <jhwlee94@gmail.com>
2022-02-09 21:06:01 -0800

    add animations to technologies component

--- src/components/Technologies/Technologies.tsx
+++ src/components/Technologies/Technologies.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import styles from './Technologies.module.css';
 import js from './img/js.png';
 import python from './img/python.png';
@@ -11,51 +13,51 @@ import sql from './img/sql.png';
 import ts from './img/ts.png';
 
 function Technologies() {
+    useEffect(() => {
+        AOS.init({ duration: 500 });
+    }, []);
+
     return (
         <div className={styles.techContainer} id='skills'>
             <h1 className={styles.techHeading}>LANGUAGES</h1>
             <div className={styles.languages}>
                 <div className={styles.iconContainer}>
-                    <img src={js} alt="JavaScript Icon" className={styles.techIcons} />
+                    <img src={js} alt="JavaScript Icon" className={styles.techIcons} data-aos='zoom-in' data-aos-once='true' />
                     <h2 className={styles.techHeader}>JavaScript</h2>
                 </div>
                 <div className={styles.iconContainer}>
-                    <img src={ts} alt="Typescript Icon" className={styles.techIcons} />
+                    <img src={ts} alt="Typescript Icon" className={styles.techIcons} data-aos='zoom-in' data-aos-once='true' />
                     <h2 className={styles.techHeader}>TypeScript</h2>
                 </div>
                 <div className={styles.iconContainer}>
-                    <img src={python} alt="Python Icon" className={styles.techIcons} />
+                    <img src={python} alt="Python Icon" className={styles.techIcons} data-aos='zoom-in' data-aos-once='true' />
                     <h2 className={styles.techHeader}>Python</h2>
                 </div>
                 <div className={styles.iconContainer}>
-                    <img src={html} alt="HTML Icon" className={styles.techIcons} />
+                    <img src={html} alt="HTML Icon" className={styles.techIcons} data-aos='zoom-in' data-aos-once='true' />
                     <h2 className={styles.techHeader}>HTML</h2>
                 </div>
                 <div className={styles.iconContainer}>
-                    <img src={css} alt="CSS Icon" className={styles.techIcons} />
+                    <img src={css} alt="CSS Icon" className={styles.techIcons} data-aos='zoom-in' data-aos-once='true' />
                     <h2 className={styles.techHeader}>CSS</h2>
                 </div >
             </div>
             <h1 className={styles.techHeading2}>FRAMEWORKS</h1>
             <div className={styles.frameworks}>
                 <div className={styles.iconContainer}>
-                    <img src={react} alt="React Icon" className={styles.techIcons} />
+                    <img src={react} alt="React Icon" className={styles.techIcons} data-aos='zoom-in' data-aos-once='true' />
                     <h2 className={styles.techHeader}>React</h2>
                 </div>
-                {/* <div>
-                    <img src={css} alt="CSS Icon" />
-                    <h2>React Router</h2>
-                </div> */}
                 <div className={styles.iconContainer}>
-                    <img src={mongodb} alt="MongoDB Icon" className={styles.techIcons} />
+                    <img src={mongodb} alt="MongoDB Icon" className={styles.techIcons} data-aos='zoom-in' data-aos-once='true' />
                     <h2 className={styles.techHeader}>MongoDB</h2>
                 </div>
                 <div className={styles.iconContainer}>
-                    <img src={express} alt="Express Icon" className={styles.techIcons} />
+                    <img src={express} alt="Express Icon" className={styles.techIcons} data-aos='zoom-in' data-aos-once='true' />
                     <h2 className={styles.techHeader}>Express</h2>
                 </div>
                 <div className={styles.iconContainer}>
-                    <img src={sql} alt="SQL Icon" className={styles.techIcons} />
+                    <img src={sql} alt="SQL Icon" className={styles.techIcons} data-aos='zoom-in' data-aos-once='true' />
                     <h2 className={styles.techHeader}>SQL</h2>
                 </div>
             </div>
