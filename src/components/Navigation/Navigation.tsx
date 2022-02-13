48505eac8f96153d594c00ce362d20f89faf1acf
jeremiahlee94 <jhwlee94@gmail.com>
2022-02-12 23:22:26 -0800

    add downloadable pdf

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,6 +3,7 @@ import github from './img/github.png';
 import linkedIn from './img/linkedin.png';
 import gmail from './img/gmail.png';
 import styles from './Navigation.module.css';
+import jlee from './file/jeremiahlee.pdf';
 
 function Navigation() {
     const [showNav, setshowNav] = useState(false);
@@ -27,7 +28,7 @@ function Navigation() {
                             <a href='#skills' className={styles.aTagNav}><li className={styles.jumpLink}>SKILLS</li></a>
                             <a href='#projects' className={styles.aTagNav}><li className={styles.jumpLink}>PROJECTS</li></a>
                             <a href='#about' className={styles.aTagNav}><li className={styles.jumpLink}>ABOUT</li></a>
-                            <a href='resume/jleeresume.pdf' download='Jeremiah_Lee_Resume' target='_blank' rel='noreferrer' className={styles.aTagNav}><li className={styles.resumeLink}>CV</li></a>
+                            <a href={jlee} download='Jeremiah_Lee_Resume' target='_blank' rel='noreferrer' className={styles.aTagNav}><li className={styles.resumeLink}>CV</li></a>
                             <li className={styles.otherLink}>
                                 <a href='https://github.com/leejoonli' target='_blank' rel='noreferrer' className={styles.aTag}><img src={github} alt="Link to Jeremiah's Github" className={styles.imgLink} /></a>
                                 <a href='https://www.linkedin.com/in/jhwlee/' target='_blank' rel='noreferrer' className={styles.aTag}><img src={linkedIn} alt="Link to Jeremiah's LinkedIn" className={styles.imgLink} /></a>
